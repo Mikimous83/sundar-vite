7717fe17430a341b1c9477ad48f9583a43ee4999
Mikimous83 <chatgpt.mouse@gmail.com>
2024-05-18 09:05:41 -0500

    avance navar

---
+++ src/header/Submenu/submenuLatopsNoteBooks.js
@@ -0,0 +1,54 @@
+import laptopsImage from "./assets/LAPTOPS.png"
+const submenuLatopsNoteBooks = {
+    title: "Laptops y NoteBooks",
+    link: "/Laptops y Notebooks",
+    image: laptopsImage,
+    columns: [
+        {
+            title: "LAPTOP",
+            items: [
+                { label: "Asus", link: "/" },
+                { label: "Hp", link: "/" },
+                { label: "Lenovo", link: "/" },
+                { label: "Dell", link: "/" },
+                { label: "Acer", link: "/" },
+            ],
+        },
+        {
+            title: "LATOP GAMER",
+            items: [
+                { label: "Asus", link: "/" },
+                { label: "Msi", link: "/" },
+                { label: "Lenovo", link: "/" },
+                { label: "Hp", link: "/" },
+                { label: "Acer", link: "/" },
+                { label: "Dell", link: "/" },
+                { label: "Gygabite", link: "/" },
+            ],
+        },
+        {
+            title: "LINEA EMPRESARIAL",
+            items: [
+                { label: "Thinkpad", link: "/" },
+                { label: "Hp Serie 2000", link: "/" },
+                { label: "Hp Serie", link: "/" },
+                { label: "ProBook", link: "/" },
+                { label: "Hp Serie", link: "/" },
+                { label: "EliteBook", link: "/" },
+                { label: "Dell Vostro", link: "/" },
+            ],
+        },
+        {
+            title: "ALL IN ONE",
+            items: [
+                { label: "Hp", link: "/" },
+                { label: "Lenovo", link: "/" },
+                { label: "Acer", link: "/" },
+            ],
+        },
+    ],
+}
+
+export default submenuLatopsNoteBooks
+
+
